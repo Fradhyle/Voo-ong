f7c90f4d19cea4981283764322008728106788a7
Donghyeok Im <fradhyle@gmail.com>
2025-10-29 14:21:20 +0900

    2025-10-29 update

--- Voo-ong apps/pylib/multi_thread_executor.py
+++ Voo-ong apps/pylib/multi_thread_executor.py
@@ -1,13 +1,13 @@
 # multi_thread_executor.py
 # 작업을 멀티스레드로 처리할 수 있도록 해주는 모듈
 
-from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor
-from tqdm import tqdm
+import os
 import random
 import re
-import requests
-import numpy as np
-import os
-import pandas as pd
 import time
+from concurrent.futures import ThreadPoolExecutor
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
